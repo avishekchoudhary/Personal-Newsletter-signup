9abc2733371b0deaf7cc4f19705e2b6b7772f0a0
Avishek Choudhary <56909326+avishekchoudhary@users.noreply.github.com>
2020-08-21 20:05:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
-require('dotenv').config()
+const require('dotenv').config();
 const app = express();
 
 app.use(express.static("public"));
