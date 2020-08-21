6fbfc973d57239939415ea66263543c69b451f13
Avishek Choudhary <achoudhary2_be19@thapar.edu>
2020-08-21 19:59:15 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+require('dotenv').config()
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+  const firstName = req.body.fname;
+  const lastName = req.body.lastname;
+  const email = req.body.email;
+  console.log(firstName);
+  console.log(lastName);
+  console.log(email);
+
+  const data = {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+      }
+    }]
+  };
+  const jsonData = JSON.stringify(data);
+const url = process.env.API_URL;
+  const options = {
+    method: "post",
+    auth: process.env.MAIL_AUTH
+  };
+
+
+  const request = https.request(url, options, function(response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html")
+    } else {
+      res.sendFile(__dirname + "/failure.html")
+    }
+
+
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
+
+app.listen(3000, function() {
+  console.log("You are up and running");
+});
